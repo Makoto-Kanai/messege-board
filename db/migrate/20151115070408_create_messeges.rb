2bfa838e0c6f50f1c56e98dfe5fdcec9f5dd4c8a
makoto_kanai <mako@funnys-inc.net>
2015-11-15 07:40:30 +0000

    creatmessegemedel

--- db/migrate/20151115070408_create_messeges.rb
+++ db/migrate/20151115070408_create_messeges.rb
@@ -2,8 +2,7 @@ class CreateMesseges < ActiveRecord::Migration
   def change
     create_table :messeges do |t|
       t.string :name
-      t.strings :body
-
+      t.string :body
       t.timestamps null: false
     end
   end
