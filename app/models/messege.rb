61b07c0be2a909b56c8cb1550b19dc2075dcdea6
makoto_kanai <mako@funnys-inc.net>
2015-11-18 07:37:08 +0000

    create messege model

--- app/models/messege.rb
+++ app/models/messege.rb
@@ -1,2 +1,10 @@
 class Messege < ActiveRecord::Base
+    def change
+        create_table :messeges do |t|
+            t.string :name
+            t.string :body
+            
+            t.timestamps null: false
+        end
+    end
 end
