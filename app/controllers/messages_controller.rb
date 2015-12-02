f4ee3815d71a04d90384d12564897711d7cd50ac
makoto_kanai <mako@funnys-inc.net>
2015-12-02 11:53:55 +0000

    comprite 8.7

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,11 +29,30 @@ class MessagesController < ApplicationController
     redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+    
+  
+  def edit
+    
+  end
+  
   
   private
   
     def message_params
       params.require(:message).permit(:name, :body)
     end
-    
+    #テキストで確認
+    def set_message 
+      @message = Message.find(params[:id])
+    end
 end
